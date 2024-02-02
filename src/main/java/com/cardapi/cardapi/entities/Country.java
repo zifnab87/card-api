c1af7dcf7a3aabdd61c62f9d7febe6c75db88cb8
Michail Michailidis <mmichaelid@gmail.com>
2024-02-02 02:47:20 +0200

    fixed swagger examples not showing value objects properly;

--- src/main/java/com/cardapi/cardapi/entities/Country.java
+++ src/main/java/com/cardapi/cardapi/entities/Country.java
@@ -1,12 +1,14 @@
 package com.cardapi.cardapi.entities;
 
 import com.cardapi.cardapi.exceptions.InvalidIsoCodeException;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Value;
 
 import java.util.Locale;
 import java.util.Set;
 
 @Value
+@Schema(implementation = String.class)
 public class Country {
     private static final Set<String> ISO_COUNTRIES = Set.of(Locale.getISOCountries());
 
